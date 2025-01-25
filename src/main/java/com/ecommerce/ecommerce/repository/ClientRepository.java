1ba531915c74b701321cf4a34e3c9e4879998941
michaelcesar <michaelcesarflopesgit@gmail.com>
2025-01-25 13:53:49 -0300

    feat: user repository

---
+++ src/main/java/com/ecommerce/ecommerce/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.ecommerce.repository;
+
+import com.ecommerce.ecommerce.domain.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByEmail(String cpf);
+}
---
+++ src/main/java/com/ecommerce/ecommerce/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.ecommerce.repository;
+
+import com.ecommerce.ecommerce.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
