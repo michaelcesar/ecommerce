62b92ab771179b0b901794ff752f7ad90fa8cc4b
michaelcesar <michaelcesarflopesgit@gmail.com>
2025-01-25 12:52:50 -0300

    feat: role entity

---
+++ src/main/java/com/ecommerce/ecommerce/entities/Role.java
@@ -0,0 +1,21 @@
+package com.ecommerce.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+}
