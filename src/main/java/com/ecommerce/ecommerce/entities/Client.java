74fb181cf0ae324a91b805939af6d5c4ac3edf85
michaelcesar <michaelcesarflopesgit@gmail.com>
2025-01-25 12:12:56 -0300

    feat: client entity

---
+++ src/main/java/com/ecommerce/ecommerce/entities/Client.java
@@ -0,0 +1,41 @@
+package com.ecommerce.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String address;
+
+    @Column
+    private String cpf;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+   /* @OneToOne(mappedBy = "client")
+    private Adress adress;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();*/
+    }
